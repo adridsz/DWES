81b50277572b75402a6de48c32b070d160353140
Adrian Diaz <adiazs22@fpcoruna.afundacion.org>
2023-09-27 09:43:54 +0200

    Adivinanza ejercicio 2 completada

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,18 +1,50 @@
-from tkinter import Variable
-
+puntos = 0
 
 print("La A, anda. La B, besa. La C, reza. ¿Qué fruta es esa?")
 print("A- Pera")
 print("B- Cereza")
 print("C- Manzana")
-
 while True:
     respuesta = input("Introduce la respuesta(A,B o C):" ).lower()
     if respuesta=="a" or respuesta=="b"or respuesta=="c":
         break
 if respuesta=="b":
     print("La respuesta es correcta")
+    puntos+=10
+
 else:
     print("Respuesta incorrecta")
+    puntos-=5
 
-    
+print("Soy bonito por delante, algo feo por detrás, me transformo a cada instante, ya que imito a los demás. ")
+print("A- Espejo")
+print("B- Titere")
+print("C- Farola")
+while True:
+    respuesta = input("Introduce la respuesta(A,B o C):" ).lower()
+    if respuesta=="a" or respuesta=="b"or respuesta=="c":
+        break
+if respuesta=="a":
+    print("La respuesta es correcta")
+    puntos+=10
+
+else:
+    print("Respuesta incorrecta")
+    puntos-=5
+
+print("Ruedo y ruedo, y en los bolsillos me quedo.")
+print("A- Cartera")
+print("B- Llaves")
+print("C- Moneda")
+while True:
+    respuesta = input("Introduce la respuesta(A,B o C):" ).lower()
+    if respuesta=="a" or respuesta=="b"or respuesta=="c":
+        break
+if respuesta=="c":
+    print("La respuesta es correcta")
+    puntos+=10
+
+else:
+    print("Respuesta incorrecta")
+    puntos-=5
+print("Obtuviste ",puntos," de 30 posibles")
