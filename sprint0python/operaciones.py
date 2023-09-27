443668fd56de941c0ae169a9873d54e4431cefa4
Adrian Diaz <adiazs22@fpcoruna.afundacion.org>
2023-09-27 10:15:47 +0200

    Calculadora ejercicio 3 completado

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,12 @@
+def suma (num, num2):
+    return num + num2
+
+def resta (num, num2):
+    return num-num2
+def multiplicacion (num, num2):
+    return num*num2
+def division (num, num2):
+    if num2==0:
+        return "No se puede dividir entre 0!!!"
+    else:
+        return num/num2
