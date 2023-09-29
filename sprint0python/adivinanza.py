820aad705064f53299bb10443b991813ec488506
Adrian Diaz <adiazs22@fpcoruna.afundacion.org>
2023-09-29 13:55:34 +0200

    Continuar en casa

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,48 +1,29 @@
-puntos = 0
+import random
 
-print("La A, anda. La B, besa. La C, reza. ¿Qué fruta es esa?")
-print("A- Pera")
-print("B- Cereza")
-print("C- Manzana")
+
+p = 0
+v = 0
+a = {
+    1: "La A, anda. La B, besa. La C, reza. ¿Qué fruta es esa? (A- Pera  B- Cereza  C- Manzana)",
+    2: "Soy bonito por delante, algo feo por detrás, me transformo a cada instante, ya que imito a los demás. (A- Espejo  B- Titere  C- Farola)",
+    3: "Ruedo y ruedo, y en los bolsillos me quedo.(A- Cartera  B- Llaves  C- Moneda)"
+}
+
+ca=list(a.keys())
+while True:
+    adivinanza = random.sample(ca, 1) [0]
+    ca.remove(adivinanza)
+    
+    print(ca[adivinanza])
+    respuesta=input()
+    v+=1
+    if v==3:
+        break
+
 while True:
     respuesta = input("Introduce la respuesta(A,B o C):" ).lower()
-    if respuesta=="a" or respuesta=="b"or respuesta=="c":
+    if respuesta in ["a", "b", "c"]:
         break
-if respuesta=="b":
-    print("La respuesta es correcta")
-    puntos+=10
-
-else:
-    print("Respuesta incorrecta")
-    puntos-=5
-
-print("Soy bonito por delante, algo feo por detrás, me transformo a cada instante, ya que imito a los demás. ")
-print("A- Espejo")
-print("B- Titere")
-print("C- Farola")
-while True:
-    respuesta = input("Introduce la respuesta(A,B o C):" ).lower()
-    if respuesta=="a" or respuesta=="b"or respuesta=="c":
-        break
-if respuesta=="a":
-    print("La respuesta es correcta")
-    puntos+=10
-
-else:
-    print("Respuesta incorrecta")
-    puntos-=5
-
-print("Ruedo y ruedo, y en los bolsillos me quedo.")
-print("A- Cartera")
-print("B- Llaves")
-print("C- Moneda")
-while True:
-    respuesta = input("Introduce la respuesta(A,B o C):" ).lower()
-    if respuesta=="a" or respuesta=="b"or respuesta=="c":
-        break
-if respuesta=="c":
-    print("La respuesta es correcta")
-    puntos+=10
 
 else:
     print("Respuesta incorrecta")
