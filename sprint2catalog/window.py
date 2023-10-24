0334ee296867b7986e1e2e218ad489d9200aa7fb
Adrian Diaz <adiazs22@fpcoruna.afundacion.org>
2023-10-24 10:16:50 +0200

    Ejercicio 7 Recuperar y mostrar información dinámicamente desde Tkinter

--- sprint2catalog/window.py
+++ sprint2catalog/window.py
@@ -1,26 +1,49 @@
 import threading
 import requests
 from PIL import Image, ImageTk
-from tkinter import Tk, ttk, messagebox
+from tkinter import Canvas, Frame, Label, Scrollbar, Tk, ttk, messagebox
 import tkinter as tk
 from cell import Cell
 from detail_window import mostrar_detalles
 
 class MainWindow():
-    cells = [] # Aqui creamos una lista que rellenaremos con los datos del json
+    cells = []
+    
     def __init__(self, root, json_data):
-        #Titulo ventana
-        root.title("MainWindow") # Le damos el titulo a la ventana principal
-        
-        for paisaje in json_data: # Con este bucle metemos los datos del json en la lista cells
+        root.title("MainWindow")
+        for paisaje in json_data:
             nombre = paisaje.get("name")
             descripcion = paisaje.get("description")
             url = paisaje.get("image_url")
             cell = Cell(nombre, url, descripcion)
             self.cells.append(cell)
         
-        #Bucle para ver las imagenes
+        # SCROLLBAR
+        self.canvas = tk.Canvas(root, highlightthickness=0)
+        self.scrollbar = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview, width=15)
+        
+        # Creamos el frame del scroll scroll
+        self.scrollable_frame = tk.Frame(self.canvas)
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+        
+        #Configuramos el scroll en el canvas
+        self.canvas.create_window((0,0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        
         for i, cell in enumerate(self.cells):
-            label = ttk.Label(root, image= cell.image_tk, text= cell.nombre, compound=tk.BOTTOM)
+            frame = tk.Frame(self.scrollable_frame)
+            frame.pack(pady=10)
+            label = tk.Label(frame, image=cell.image_tk, text=cell.nombre, compound=tk.BOTTOM)
             label.grid(row=i, column=0)
             label.bind("<Button-1>", lambda event, celda= cell: mostrar_detalles(celda))
+        
+        #Configuramos la posicion del scroll en la ventana
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.scrollbar.grid(row=0, column=1, sticky="ns")
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)
